d714aadb82781cb0deea2f4da540c7232fd1f99b
Arvind-17-svg <raghuraman.arv@northeastern.edu>
2025-04-18 01:19:33 -0400

    Updating Frontend

--- frontend.py
+++ frontend.py
@@ -6,7 +6,7 @@ from io import StringIO
 from datetime import datetime
 # Config
 #BACKEND_URL = "http://localhost:8000"
-BACKEND_URL = "https://final-project-deploy-343736309329.us-central1.run.app"
+BACKEND_URL = "https://final-project-deploy-343736309329.us-central1.run.app/"
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
